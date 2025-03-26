03aae7c24202435bf6ecd31668867ac5f30c85da
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-03-26 12:43:50 +0100

    Legg til client island

--- src/components/client-island/ClientIsland.tsx
+++ src/components/client-island/ClientIsland.tsx
@@ -1,16 +1,14 @@
 import { Button, Heading } from "@navikt/ds-react";
 
 const ClientIsland = () => {
-    return (
-        <div>
-            <Heading size="small" level="3">
-                Client-side component
-            </Heading>
-            <Button onClick={() => console.log("clicked")}>
-                Button
-            </Button>
-        </div>
-    );
+  return (
+    <div>
+      <Heading size="small" level="3">
+        Client-side component
+      </Heading>
+      <Button onClick={() => console.log("clicked")}>Button</Button>
+    </div>
+  );
 };
 
 export default ClientIsland;
